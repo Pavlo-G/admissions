702c46a55532c48a27036289862f53521b9c9bb1
Pavlo-G <6306260@gmail.com>
2020-10-01 19:59:43 +0300

    first commit with users

---
+++ src/main/java/com/training/admissions/model/Status.java
@@ -0,0 +1,5 @@
+package com.training.admissions.model;
+
+public enum Status {
+    ACTIVE,BANNED;
+}
---
+++ src/main/java/com/training/admissions/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.training.admissions.security;
+
+import com.training.admissions.model.User;
+import com.training.admissions.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User does't exists"));
+        return SecurityUser.fromUser(user);
+    }
+}
